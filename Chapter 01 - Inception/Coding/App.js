679a1f9e7fa0c18714cdaa56f0411b768ec4d5b8
aayush patidar <ap6887980@gmail.com>
2025-01-15 15:20:26 +0530

    in depth foundation of react and our episode-1st is done

--- Chapter 01 - Inception/Coding/App.js
+++ Chapter 01 - Inception/Coding/App.js
@@ -1,5 +1,33 @@
-const heading = React.createElement("h1", {}, "Hello World from react");
+/**
+ *
+ * <div id="parent">
+ *     <div id="child">
+ *           <h1>i am h1 tag</h1> h1 and h2 are siblings inside the child, and this siblings for using array
+ *           <h2>i am h2 tag</h2>
+ *     </div>
+ * </div>
+ *     <div id="child2">
+ *           <h1>i am h1 tag</h1>
+ *           <h2>i am h2 tag</h2>
+ *     </div>
+ *
+ */
+
+const heading = React.createElement(
+  "div",
+  { id: "parent", xyz: "abc" }, // this is id and xyz attribute and this is props and child, it is inside in his parent h1
+  [
+    React.createElement("div", { id: "child" }, [
+      React.createElement("h1", {}, "i am h1 tag"),
+      React.createElement("h2", {}, "i am h2 tag"),
+    ]),
+    React.createElement("div", { id: "child2" }, [
+      React.createElement("h1", {}, "i am h1 tag"),
+      React.createElement("h2", {}, "i am h2 tag"),
+    ]),
+  ]
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(heading); // this render method conver heading object in h1 tag and put
